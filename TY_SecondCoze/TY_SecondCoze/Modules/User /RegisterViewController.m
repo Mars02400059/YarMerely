3c831114a890af38435bea0d3fae91a980c5ed02
Mars02400059 <Mars02400059@163.com>
2016-10-20 19:40:10 +0800

    wang:

--- TY_SecondCoze/TY_SecondCoze/Modules/User /RegisterViewController.m
+++ TY_SecondCoze/TY_SecondCoze/Modules/User /RegisterViewController.m
@@ -17,11 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 /*
